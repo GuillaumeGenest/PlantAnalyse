6aa32e2abd79fbdc11a534a896e5d74d1e9f5ff9
Guillaume Genest <guillaume-genest2@hotmail.fr>
2023-03-28 15:51:33 +0200

    Initial Commit

---
+++ PlantAnalyse/PlantAnalyseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantAnalyseApp.swift
+//  PlantAnalyse
+//
+//  Created by Guillaume Genest on 28/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct PlantAnalyseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
